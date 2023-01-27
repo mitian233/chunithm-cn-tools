8f608381b5f33e8b6c2409b573a6420d477dac0c
mitian233 <2304254165@qq.com>
2023-01-27 11:56:10 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build:{
+    target: 'esnext',
+  },
   chainWebpack: config => {
     config.plugin('html').tap(args => {args[0].title='CHUNITHM CN 工具箱';return args})
   },

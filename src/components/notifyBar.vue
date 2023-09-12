6f95de017f1e648150ef0294a99ce2d8279a9786
Aaki <mitian233@yahoo.co.jp>
2023-09-12 11:28:13 +0000

    Changes to be committed: 	new file:   bun.lockb 	modified:   src/components/notifyBar.vue 	modified:   yarn.lock

--- src/components/notifyBar.vue
+++ src/components/notifyBar.vue
@@ -33,7 +33,6 @@ watch(showNotifyBar, (newVal) => {
       class="mb-2"
       closable>
     <p>本站将于近期搬迁到新的域名<a href="https://chu.3kn.jp">chu.3kn.jp</a>，请窒息。</p>
-    <p>该域名下网站无广告，但是可能会有一些不稳定，如果有问题请联系开发者。</p>
   </v-alert>
 </template>
 
